3d8497f1690b9d1484126fce962346fd3492c9bb
Deni <liquid.yethee@hotmail.com>
2011-12-05 22:24:19 +0400

    Updated fixtures for the tests

--- Tests/Fixtures/ExtendedSimpleEnum.php
+++ Tests/Fixtures/ExtendedSimpleEnum.php
@@ -6,21 +6,4 @@ use Biplane\EnumBundle\Enumeration\Enum;
 
 class ExtendedSimpleEnum extends SimpleEnum
 {
-    const ZERO   = 0;
-    const FIRST  = 1;
-    const SECOND = 2;
-
-    public static function getReadables()
-    {
-        return array(
-            self::ZERO => 'Zero',
-            self::FIRST => 'First',
-            self::SECOND => 'Second'
-        );
-    }
-
-    public static function getPossibleValues()
-    {
-        return array(self::ZERO, self::FIRST, self::SECOND);
-    }
 }
